6dd3731104b4f3d310543d0221abf81757d4e1cb
Leon Schuermann <leon@is.currently.online>
2025-02-10 14:41:13 -0500

    nixos/tests/systemd-resolved: test resolving A and AAAA of own FQDN
    This adds a test that ensures that hosts can resolve their own v4 and
    v6 addresses as encoded in A and AAAA records of their upstream DNS
    server or resolver respectively.
    
    This test fails on this commit with a regular NixOS install, which
    contains an `/etc/hosts` mapping for FQDN to 127.0.0.2, but no such
    mapping for an IPv6 loopback IP (as there are no multiple loopback v6
    IPs, and each address must only be listed once [1]). While for NixOS
    test configurations this test passes, this is only because the test
    networking module adds additional entries to `networking.extraHosts`
    which are not present in a regular NixOS configuration.
    
    [1]: https://github.com/NixOS/nixpkgs/pull/362132

--- nixos/tests/systemd-resolved.nix
+++ nixos/tests/systemd-resolved.nix
@@ -1,15 +1,23 @@
-{ pkgs, lib, ... }:
+
+  { pkgs, lib, ... }:
 {
   name = "systemd-resolved";
   meta.maintainers = [ lib.maintainers.elvishjerricco ];
 
   nodes.server =
-    { lib, config, ... }:
+    {
+      lib,
+      config,
+      nodes,
+      ...
+    }:
     let
       exampleZone = pkgs.writeTextDir "example.com.zone" ''
         @ SOA ns.example.com. noc.example.com. 2019031301 86400 7200 3600000 172800
         @       A       ${(lib.head config.networking.interfaces.eth1.ipv4.addresses).address}
         @       AAAA    ${(lib.head config.networking.interfaces.eth1.ipv6.addresses).address}
+        client  A       ${(lib.head nodes.client.networking.interfaces.eth1.ipv4.addresses).address}
+        client  AAAA    ${(lib.head nodes.client.networking.interfaces.eth1.ipv6.addresses).address}
       '';
     in
     {
@@ -37,11 +45,20 @@
     };
 
   nodes.client =
-    { nodes, ... }:
+    { lib, nodes, ... }:
     let
       inherit (lib.head nodes.server.networking.interfaces.eth1.ipv4.addresses) address;
     in
     {
+      networking.hostName = "client";
+      networking.domain = "example.com";
+
+      # The NixOS testing framework automatically generates host entries for
+      # the `hostName` and FQDN settings pointing to the nodes' primary
+      # IP. Instead, we want to test how a real, unmodified NixOS system will
+      # behave. Override the `extraHosts` setting accordingly:
+      networking.extraHosts = lib.mkForce "";
+
       networking.nameservers = [ address ];
       networking.interfaces.eth1.ipv6.addresses = lib.mkForce [
         {
@@ -67,8 +84,10 @@
   testScript =
     { nodes, ... }:
     let
-      address4 = (lib.head nodes.server.networking.interfaces.eth1.ipv4.addresses).address;
-      address6 = (lib.head nodes.server.networking.interfaces.eth1.ipv6.addresses).address;
+      serverAddress4 = (lib.head nodes.server.networking.interfaces.eth1.ipv4.addresses).address;
+      serverAddress6 = (lib.head nodes.server.networking.interfaces.eth1.ipv6.addresses).address;
+      clientAddress4 = (lib.head nodes.client.networking.interfaces.eth1.ipv4.addresses).address;
+      clientAddress6 = (lib.head nodes.client.networking.interfaces.eth1.ipv6.addresses).address;
     in
     ''
       start_all()
@@ -76,10 +95,18 @@
 
       def test_client():
           query = client.succeed("resolvectl query example.com")
-          assert "${address4}" in query
-          assert "${address6}" in query
+          assert "${serverAddress4}" in query
+          assert "${serverAddress6}" in query
           client.succeed("ping -4 -c 1 example.com")
           client.succeed("ping -6 -c 1 example.com")
+
+          # Ensure that the client can resolve its own FQDN and retreive both
+          # A and AAAA records:
+          query = client.succeed("resolvectl query client.example.com")
+          assert "${clientAddress4}" in query
+          assert "${clientAddress6}" in query
+          client.succeed("ping -4 -c 1 client.example.com")
+          client.succeed("ping -6 -c 1 client.example.com")
 
       client.wait_for_unit("initrd.target")
       test_client()
